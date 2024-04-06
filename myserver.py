f6f271eb6db748ca6f5c75b97a47b64de1a5122c
rily <18514223+tungtylee@users.noreply.github.com>
2024-04-06 22:17:29 +0800

    feat: use keyboard for stopping recording
    use keyboard and stop queue for trigger stop recording
    
    original stop route cannot work, because recording thread does not
    receive stop message

--- myserver.py
+++ myserver.py
@@ -1,8 +1,10 @@
+import queue
 from threading import Thread
 
 import sounddevice as sd
 import soundfile as sf
 from flask import Flask, jsonify
+from pynput import keyboard
 
 app = Flask(__name__)
 
@@ -12,11 +14,42 @@ fs = 44100  # sampling rate
 duration = 60  # max duration
 
 
+# a queue for stopping recording
+stop_queue = queue.Queue(maxsize=1)
+
+
+def clear_queue(q):
+    while not q.empty():
+        try:
+            q.get_nowait()
+        except queue.Empty:
+            break
+
+
+def on_press(key):
+    global stop_queue
+    if key == keyboard.Key.space:
+        stop_queue.put(True)
+
+
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+
+
 def start_recording():
-    global is_recording, recording
+    global is_recording, recording, stop_queue
+    clear_queue(stop_queue)
     is_recording = True
     recording = sd.rec(int(duration * fs), samplerate=fs, channels=2, dtype="int16")
     print("Recording ...")
+    try:
+        stop_queue.get(timeout=duration)  # wait key
+    except queue.Empty:
+        # max duration
+        pass
+    finally:
+        print("Recording st opped.")
+        stop_recording_and_save()
 
 
 def stop_recording_and_save():
@@ -39,12 +72,6 @@ def trigger_recording():
         return jsonify({"status": "Recording started"}), 200
     else:
         return jsonify({"status": "Already recording"}), 200
-
-
-@app.route("/stop_recording", methods=["GET", "POST"])
-def handle_stop():
-    stop_recording_and_save()
-    return jsonify({"status": "Recording stopped and saved"}), 200
 
 
 if __name__ == "__main__":
