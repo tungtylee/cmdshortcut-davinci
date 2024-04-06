9c1b111f90e9eb6fad119fc81c4d5cb16fe68a99
rily <18514223+tungtylee@users.noreply.github.com>
2024-04-06 22:43:44 +0800

    feat: add whisper.cpp and exec asr when recording done

--- myserver.py
+++ myserver.py
@@ -1,3 +1,4 @@
+import os
 import queue
 from threading import Thread
 
@@ -10,9 +11,11 @@ app = Flask(__name__)
 
 is_recording = False
 recording = None
-fs = 44100  # sampling rate
+fs = 16000  # sampling rate
 duration = 60  # max duration
-
+WHISPER_M = "./whisper.cpp/main -m ./whisper.cpp/models/ggml-large-v2.bin"
+WHISPER_F = " -f recording.wav -otxt recording.txt"
+WHISPER_CMD = WHISPER_M + WHISPER_F
 
 # a queue for stopping recording
 stop_queue = queue.Queue(maxsize=1)
@@ -41,7 +44,8 @@ def start_recording():
     clear_queue(stop_queue)
     is_recording = True
     recording = sd.rec(int(duration * fs), samplerate=fs, channels=2, dtype="int16")
-    print("Recording ...")
+    print("Recording after beep ...")
+
     try:
         stop_queue.get(timeout=duration)  # wait key
     except queue.Empty:
@@ -61,8 +65,15 @@ def stop_recording_and_save():
         filename = "recording.wav"
         sf.write(filename, recording, fs, subtype="PCM_16")
         print(f"Save to {filename}.")
+        exec_whisper()
     else:
         print("No recording in progress")
+
+
+def exec_whisper():
+    print(WHISPER_CMD)
+    os.system(WHISPER_CMD)
+    os.system("gedit recording.wav.txt")
 
 
 @app.route("/start_recording", methods=["GET", "POST"])
@@ -72,6 +83,12 @@ def trigger_recording():
         return jsonify({"status": "Recording started"}), 200
     else:
         return jsonify({"status": "Already recording"}), 200
+
+
+@app.route("/stop_recording", methods=["GET", "POST"])
+def handle_stop():
+    stop_recording_and_save()
+    return jsonify({"status": "Recording stopped and saved"}), 200
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -8,6 +8,14 @@
 python myserver.py
 ```
 
+* Record and ASE
+
+```
+# browse http://127.0.0.1:5000/start_recording
+# press space to stop recording
+```
+
+
 ## Installation
 
 * conda environment and development
@@ -28,3 +36,39 @@ pip install soundfile
 # ImportError: NumPy must be installed for play()/rec()/playrec
 pip install numpy
 ```
+
+## Whisper Installation
+
+* Clone and Download
+
+```bash
+git clone https://github.com/ggerganov/whisper.cpp.git
+cd whisper
+bash ./models/download-ggml-model.sh large-v2
+make clean
+```
+
+* Setting
+  * Install cuda to `/usr/local/cuda`
+  * Add path and ld in `~/.bashrc`
+  * Modify `Makefile`
+  * Build
+
+  ```bash
+  # download and install cuda (an example 11.8)
+  # you can go to nvidia offical site
+  wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.runsudo sh cuda_11.8.0_520.61.05_linux.run
+
+  # ~/.bashrc
+  export PATH=/usr/local/cuda/bin:$PATH
+  export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+  # Makefile (e.g. sm_86 card)
+  # please also refer to issues 876
+  # https://github.com/ggerganov/whisper.cpp/issues/876
+  CUDA_ARCH_FLAG = sm_86
+
+  # bash command
+  make clean
+  WHISPER_CUDA=1 make -j
+  ```
